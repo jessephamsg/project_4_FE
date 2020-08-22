a85ba61902ba66302cfd92004a091755339ff17d
Linawaty <linawatylim@gmail.com>
2020-08-22 12:40:39 +0800

    create template and routing for whackamole page

---
+++ src/RoutePage/Gameboard/WhackAMole/WhackAMole.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class WhackAMole extends Component {
+    render() {
+        return (
+            <div>
+                This is WhackAMoleGame
+            </div>
+        )
+    }
+}
+
+export default WhackAMole
